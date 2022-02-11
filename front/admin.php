ae9aa4dc1bd5f8ef76fe4e1143e8369b45264867
yo76317 <yo76317@gmail.com>
2022-02-11 11:49:44 +0800

    管理登入頁面

--- front/admin.php
+++ front/admin.php
@@ -1,1 +1,15 @@
-admin
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="ans" id="ans"></td>
+    </tr>
+</table>
+<div class="ct"><button onclick="login()">確認</button></div> 
--- front/login.php
+++ front/login.php
@@ -6,23 +6,15 @@
 <table class="all">
     <tr>
         <td class="tt ct">帳號</td>
-        <td class="pp">
-            <input type="text" name="acc" id="acc">
-        </td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
     </tr>
     <tr>
         <td class="tt ct">密碼</td>
-        <td class="pp">
-            <input type="password" name="pw" id="pw">
-        </td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
     </tr>
     <tr>
         <td class="tt ct">驗證碼</td>
-        <td class="pp">
-            <input type="text" name="ans" id="ans">
-        </td>
+        <td class="pp"><input type="text" name="ans" id="ans"></td>
     </tr>
 </table>
-<div class="ct">
-    <button>確認</button>
-</div>
+<div class="ct"><button onclick="login()">確認</button></div> 
